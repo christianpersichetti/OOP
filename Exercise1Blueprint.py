7c77cc9f3506a39de7c0f72c03d342b8c2231150
christianpersichetti <123416098+christianpersichetti@users.noreply.github.com>
2023-02-13 15:42:46 -0600

    2/13 Class

--- Exercise1Blueprint.py
+++ Exercise1Blueprint.py
@@ -1,27 +1,21 @@
 import random
 #THE BLUEPRINT, USED TO CREATE A CLASS
 
-
 class Insect:
-    # The _ _init_ _ method initializes the
-    # sideup data attribute with 'Heads'.
-
-    def __init__(insect, wings, legs, flight):
-        insect.flight = 0
-        insect.wings = 2
-        insect.legs = 4
-
-
-    def toss(insect):
-        if random.rand(0,2,4) == 2:
-             insect.wings = 'The insect has 2 wings'
-        elif random.rand(0,2,4) == 4:
-             insect.legs = 'The insect has 4 legs'
-        else:
-             insect.fligt = 'flight is ____________'
-             
+    def __init__(self,n,w,l):
+        self.name = n
+        self.wings = w
+        self.legs = l
+        self.flight = 0
+        
+        
+    def flight_length(self):
+        self.flight = random.randint(1,10)
 
 
-
-    def get_sideup(insect):
-            return insect.wings, insect.legs, insect.flight
+    def get_miles(self):
+        return self.flight
+    
+    def get_name(self):
+        return self.name
+    
